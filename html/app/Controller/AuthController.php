62270f5e1bd20bf288e7357c455f95f5cb98e193
luigi-raynel-dev <devluigiraynel@gmail.com>
2025-02-13 09:27:04 -0300

    feat: create validation for sing-in route

--- html/app/Controller/AuthController.php
+++ html/app/Controller/AuthController.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Hyperf\HttpServer\Contract\ResponseInterface;
+use App\Service\UserService;
+
 /**
  * @AuthController()
  */
 class AuthController extends AbstractController
 {
-  public function signIn()
+  public function signIn(ResponseInterface $response, UserService $service)
   {
     $data = $this->request->all();
+
+    if (!$service->validate('singIn', $data)) return $response->json([
+      'message' => 'Login failed',
+      'error' => 'validation-failure',
+      'errors' => $service->validator->errors()->messages()
+    ])->withStatus(422);
 
     return [
       'message' => 'Login realizado!',
@@ -21,6 +30,7 @@ class AuthController extends AbstractController
 
   public function signUp()
   {
+
     return ['message' => 'Usuário registrado com sucesso!'];
   }
 
