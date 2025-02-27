6be8f104b546b01b5bbd12c1ea52ac11a8f7b603
luigi-raynel-dev <devluigiraynel@gmail.com>
2025-02-27 08:59:51 -0300

    feat: kafka

--- html/app/Controller/AuthController.php
+++ html/app/Controller/AuthController.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\AuthRepository;
 use Hyperf\HttpServer\Contract\ResponseInterface;
+use App\Repository\AuthRepository;
+use App\Producer\LoggerProducer;
 use App\Request\{SignInRequest, SignUpRequest, RefreshTokenRequest};
 
 /**
@@ -16,12 +17,14 @@ class AuthController extends AbstractController
 
   private $repository;
   protected ResponseInterface $response;
+  protected LoggerProducer $producer;
 
 
-  public function __construct(ResponseInterface $response, AuthRepository $repository)
+  public function __construct(ResponseInterface $response, AuthRepository $repository, LoggerProducer $producer)
   {
     $this->repository = $repository;
     $this->response = $response;
+    $this->producer = $producer;
   }
 
   public function signIn(SignInRequest $request)
